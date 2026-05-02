23df0f69026dbcfd04b0ca02d9e6ebdd4335f3cd
Jan Dvořák <dvorakjan@users.noreply.github.com>
2026-05-03 00:48:40 +0200

    fix: default color

--- client/lib/aura-colors.js
+++ client/lib/aura-colors.js
@@ -1,4 +1,4 @@
-export const AURA_COLOR = import.meta.env.VITE_AURA_COLOR || "#7373e0";
+export const AURA_COLOR = import.meta.env.VITE_AURA_COLOR || "#1c87c9";
 
 // NOT USED CURRENTLY, but can be used in the future for a more dynamic listening color based on the main aura color
 export function deriveComplementaryColor(hexColor) {
